d82aac0a7d498622ce404b18b1a8846902b28317
Guillaume Bonet <bonet.guillaume@gmail.com>
2019-07-04 17:12:17 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Kikou />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Kikou from './components/Kikou.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    Kikou
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: $primary;
+  margin-top: 60px;
+}
+</style>
